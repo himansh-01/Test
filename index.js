14be359ba3356cf4e6f58c52a6ed17ac87c9e5ab
himansh-01 <himanshprajapati2004@gmail.com>
2024-12-28 12:22:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
+const productRoute = require('./routes/product.route.js')
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.use('/api/products', productRoute)
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Update")
+})
+
+mongoose.connect("mongodb+srv://himanshprajapati2004:9KGW5sCgMiyKSr4C@cluster0.sgitv.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("Connected to database!")
+    app.listen(3000, () => {
+        console.log("server is running on port 3000")
+    })
+})
+.catch(() => {
+    console.log("Connection failed!")
+})
